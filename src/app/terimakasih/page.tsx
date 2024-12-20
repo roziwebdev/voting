db092f826841ed531e2c7d2411f696822831f3e9
Rozi <145320304+roziwebdev@users.noreply.github.com>
2024-12-20 16:56:23 +0700

    Update page.tsx

---
+++ src/app/terimakasih/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+
+export default function Terimakasih() {
+
+
+
+
+    return (
+    <div className="flex min-h-screen items-center justify-center p-8 bg-[url('/bg.jpg')] bg-cover bg-center">
+
+            <div className="w-full">
+                <h1 className='text-2xl font-bold text-center'> Terimakasih Telah Berpartisipasi Dalam Pemilihan Ketua RT 002/RW 017</h1>
+            </div>
+    </div>
+  );
+}
